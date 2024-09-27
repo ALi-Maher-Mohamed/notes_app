85d32c6d814657f228e76406338142aa4a2e3d2c
alimaher <ali.maher0013@gmail>
2024-09-27 09:24:47 +0300

    delete note done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/helper/constant.dart';
 import 'package:note_app/helper/simple_bloc_onserver.dart';
 import 'package:note_app/views/notes_view.dart';
-
 import 'models/note_models.dart';
 
 void main() async {
